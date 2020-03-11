f36be46c28c065f835bd8657d1c4fd33546ca072
rssj <674878857@qq.com>
2020-03-11 23:03:26 +0800

    Initial commit

---
+++ baselib/src/main/java/com/chlhrssj/baselib/base/BaseApplication.java
@@ -0,0 +1,10 @@
+package com.chlhrssj.baselib.base;
+
+import androidx.multidex.MultiDexApplication;
+
+/**
+ * Create by rssj on 2020-03-11
+ * Application基类
+ */
+public class BaseApplication extends MultiDexApplication {
+}
